301b58f840719ac38ee87cbe561f74a8314f2114
biandito <bekatrivz@gmail.com>
2023-06-05 17:45:04 -0300

    update

--- projeto/script.js
+++ projeto/script.js
@@ -15,3 +15,19 @@ setInterval(()=>{
     $("#relogio").html(hora)
 
 }, 60000);
+
+$(".desktop-icon").draggable({
+  containment: ".area-de-trabalho",
+  stop: function () {
+    $(this).removeClass("selected");
+    $(this).addClass("selected");
+  }
+});
+
+$(".desktop-icon").click(function(){
+    if($(this).hasClass("selected")) {
+        $(this).removeClass("selected");
+    } else {
+        $(this).addClass("selected");
+    }
+});
