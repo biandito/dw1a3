a6890409305896f063ea0116739f104cae128317
biandito <bekatrivz@gmail.com>
2023-06-13 23:55:38 -0300

    update

--- projeto/script.js
+++ projeto/script.js
@@ -7,27 +7,24 @@ setTimeout(()=>{
 
 }, 100);
 
-setInterval(()=>{
-    var hora= new Date().toLocaleDateString("pt-BR",{
-        hour: "2-digit", minute: "2-digit"
+setInterval(() => {
+    var hora = new Date().toLocaleDateString("pt-BR", {
+        hour: "2-digit",
+        minute: "2-digit"
     });
 
-    $("#relogio").html(hora)
-
+    document.getElementById("relogio").innerHTML = hora;
 }, 60000);
 
-$(".desktop-icon").draggable({
-  containment: ".area-de-trabalho",
-  stop: function () {
-    $(this).removeClass("selected");
-    $(this).addClass("selected");
-  }
+var desktopIcons = document.querySelectorAll(".desktop-icon");
+
+desktopIcons.forEach(function(icon) {
+    icon.addEventListener("click", function() {
+        if (this.classList.contains("selected")) {
+            this.classList.remove("selected");
+        } else {
+            this.classList.add("selected");
+        }
+    });
 });
 
-$(".desktop-icon").click(function(){
-    if($(this).hasClass("selected")) {
-        $(this).removeClass("selected");
-    } else {
-        $(this).addClass("selected");
-    }
-});
